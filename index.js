31002c03544ff7c46b570bc632e86fb088b66bf8
Olena Horovenko <elena.gorovenko@gmail.com>
2024-05-10 15:45:21 +0200

    added function to check the length of the credit card number input

--- index.js
+++ index.js
@@ -14,4 +14,23 @@ const handleSubmit = (e) => {
   console.log(creditCard.value, doprava.value)
 }
 
+const checkCardValueLength = () =>
+  {
+    const cardAlert = document.querySelector('.card-alert');
+
+    if (creditCard.value.length < 16) {
+      cardAlert.classList.remove('invisibility');
+      cardAlert.innerHTML = `Číslo karty je příliš krátké. Chybí ${16 - Number(creditCard.value.length)} číslic`
+    } else if (creditCard.value.length > 16) {
+      cardAlert.classList.remove('invisibility');
+      cardAlert.innerHTML = `Číslo karty je moc dlouhé. Přebývá ${Number(creditCard.value.length) - 16} číslic`
+    } else if (creditCard.value.length === 16) {
+      cardAlert.classList.add('invisibility');
+    }
+  }
+
+
+creditCard.addEventListener('change', checkCardValueLength);
+
 formEl.addEventListener('submit', handleSubmit);
+
